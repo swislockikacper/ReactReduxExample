ea06d5e6098ed0f394d1aa892ce02d883c481aec
swislockikacper <swislocki.kacper@gmail.com>
2019-06-14 14:10:11 +0200

    added empty files and project structure

---
+++ src/components/CarForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CarForm() {
+  return <div />;
+}
+
+export default CarForm;
---
+++ src/components/CarsContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CarsContainer() {
+  return <div />;
+}
+
+export default CarsContainer;
